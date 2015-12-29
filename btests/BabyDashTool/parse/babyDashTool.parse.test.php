41292b6334b474919f308415735d467d088bc335
lingtalfi <lingtalfi@gmail.com>
2015-12-29 07:36:03 +0100

    add tests for BabyDashTool::parse

---
+++ btests/BabyDashTool/parse/babyDashTool.parse.test.php
@@ -0,0 +1,39 @@
+<?php
+
+use BabyDash\BabyDashTool;
+use PhpBeast\AuthorTestAggregator;
+use PhpBeast\PrettyTestInterpreter;
+use PhpBeast\Tool\ComparisonErrorTableTool;
+
+
+require_once "bigbang.php";
+
+
+$agg = AuthorTestAggregator::create();
+
+
+$s = <<<EEE
+- apple
+- banana
+- cherry
+EEE;
+
+
+$agg->addTest(function (&$msg, $testNumber) use ($s) {
+    $res = BabyDashTool::parse($s);
+    $expected = [
+        'apple',
+        'banana',
+        'cherry',
+    ];
+
+    $ret = ($res === $expected);
+    if (false === $ret) {
+        ComparisonErrorTableTool::collect($testNumber, $expected, $res);
+    }
+    return $ret;
+});
+
+
+PrettyTestInterpreter::create()->execute($agg);
+ComparisonErrorTableTool::display();
